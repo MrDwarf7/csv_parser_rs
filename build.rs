272500b6b517a266f196ffe489d4b3f3bdece12c
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-01-20 02:35:11 +1100

    build: add build resources

--- build.rs
+++ build.rs
@@ -2,7 +2,12 @@
 extern crate windres;
 
 fn main() -> std::io::Result<()> {
-    std::env::set_var("CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG", "true");
+    #[cfg(debug_assertions)]
+    unsafe {
+        std::env::set_var("CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG", "true");
+    }
+
+    #[cfg(not(debug_assertions))] // Release mode
     if cfg!(target_os = "windows") {
         let name = env!("CARGO_PKG_NAME");
         let vers = env!("CARGO_PKG_VERSION");
@@ -10,13 +15,17 @@ fn main() -> std::io::Result<()> {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("res/icon.ico").set_language(0x0009); // English
         // .set_language(0x0409); // English (US)
+        res.set("CompanyName", "https://github.com/MrDwarf7");
+        res.set("ProductName", name);
 
         let mut sv = vers.split('.').collect::<Vec<_>>();
         while sv.len() < 4 {
             sv.push("0");
         }
 
-        // let file_version = format!("{}, {}, {}, {}", sv[0], sv[1], sv[2], sv[3]);
+        let file_version = format!("{}, {}, {}, {}", sv[0], sv[1], sv[2], sv[3]);
+        res.set("FileVersion", file_version.as_str());
+
         // windres::Build::new()
         //     .define("THE_PROJECT", Some(format!(r#""{name}"#).as_str()))
         //     .define("THE_VERSION", Some(format!(r#""{vers}"#).as_str()))
